99e1e0b048b9215288a73b61c0e94d072f1d0bd2
Maxisthemoose <oriontothemax@gmail.com>
2020-11-30 08:43:55 -0800

    Testing buyging some stuff, test schemas etc

--- src/database/interfaces/User.ts
+++ src/database/interfaces/User.ts
@@ -1,5 +1,9 @@
 import { Document } from "mongoose";
+import Store from "./Store";
 
 export default interface User extends Document {
     uId: string;
+    prestige?: number;
+    balance?: number;
+    stores?: Store[];
 }
--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -3,6 +3,10 @@ import User from "../interfaces/User";
 
 const User = new Schema({
     uId: { type: String, required: true },
+
+    prestige: { type: Number, required: false, default: 0 },
+    balance: { type: Number, required: false, default: 1000 },
+    stores: { type: Array, required: false, default: [] },
 });
 
 export default model<User>("users", User);
